c29f8fcc5a281e8bae1d701eff7d6b0ca5371172
sireeshajangam <sireesha.jangam@auropro.com>
2023-01-24 13:16:47 +0530

    committed

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_instance" "Siri" {
+resource "aws_instance" "lakshmi" {
   ami           = "ami-06984ea821ac0a879"
   instance_type = "t2.micro"
   key_name = "meenu"
