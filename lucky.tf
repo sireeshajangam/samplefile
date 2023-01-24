c7f6344bb08a9e33f2ad054d1e8cf53ffa2977a0
sireeshajangam <sireesha.jangam@auropro.com>
2023-01-24 13:32:44 +0530

    instance launching

--- lucky.tf
+++ lucky.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "Varam" {
-  ami           = "ami-06984ea821ac0a879"
+  ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
   key_name = "meenu"
   tags = {
