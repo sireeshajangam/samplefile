1ed585544428ded95c1cde8012da265230f9f66b
sireeshajangam <sireesha.jangam@auropro.com>
2023-01-25 11:38:50 +0530

    committed

--- lucky.tf
+++ lucky.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "Varam" {
   ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
-  key_name = "meenu"
+  key_name = "vara"
   tags = {
     Name = "Demo1"
   }
